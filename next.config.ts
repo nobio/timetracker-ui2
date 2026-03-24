8eeb5fd958a4d6b9cdac45868c2422345e132b78
Gernot H. Reichel <gernot.reichel@me.com>
2026-03-24 20:28:14 +0100

    changed basePath to /timetracker

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: '/timetracker',
+  assetPrefix: '/timetracker/',
   allowedDevOrigins: ['localhost:3000', '127.0.0.1']
 };
 
